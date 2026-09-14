4ee8ed8267a8e2462e81cd6ef754e357f6f68413
Maysa0110 <718630@edu.joinville.sc.gov.br>
2026-09-14 14:28:08 +0000

    my first ommit

---
+++ main.py
@@ -0,0 +1,12 @@
+print ("ola piton sssssssssss🐍!!!!!!!!!!!!!!!!!!!!!!!!!!!! ")
+
+print ("Meu nome é Maysa")
+print ("Eu gosto de viajar na maionese e ficar olhando pra um canto especifico enquanto alguem fala de algo que eu nao tenho interesse")
+print ("Meu projeto será sobre um ateliê")
+
+nome = input("Digite seu nome:")
+idade = input("Digite sua idade:")
+valor = input("Digite o valor:")
+
+print ("Olá, " + nome + "! Seja bem-vindo ao meu ateliê!")
+print ("Você tem " + idade + " e o valor é de " + valor)
